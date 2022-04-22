f2fcd0753e919dbfdedb1cf8598d123678132e64
liudec <dec_hi@qq.com>
2022-04-22 10:15:21 +0800

    update

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ cmake ..
 make
 echo "**************************************************************************************"
 #echo "The startpoint is: $1"
-./solution $1
+./solution resource/testcase/n100.hardblocks resource/testcase/n100.pl resource/testcase/n100.nets resource/output/n100_03.floorplan 0.3
 echo "**************************************************************************************"
 cd ..
